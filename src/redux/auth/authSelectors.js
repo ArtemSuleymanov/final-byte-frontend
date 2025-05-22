bb0ea9727eb97ddbad24910de46e885328c8d41a
Valentyna1302 <122233669+Valentyna1302@users.noreply.github.com>
2025-05-22 16:14:07 +0200

    start logout and refresh

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -1,3 +1,7 @@
 export const selectUser = (state) => state.auth.user;
+
 export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
+
 export const selectToken = (state) => state.auth.accesToken;
+
+export const selectIsRefreshing = (state) => state.auth.isRefreshing;
