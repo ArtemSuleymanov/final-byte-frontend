40f1db6c342b482aa812e09ed486c5071e8d7578
MaX <maksim.shepeta@gmail.com>
2025-05-23 17:25:58 +0200

    feat: add currency #8

--- src/components/layout/Currency/Currency.jsx
+++ src/components/layout/Currency/Currency.jsx
@@ -1,7 +1,50 @@
-const Currency = () => {
-  return (
-    <div>Currency</div>
-  )
-}
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCurrencyIfNeeded } from '../../../redux/currency/currencySlice';
+import { formatValue } from '../../../utils/helpers';
 
-export default Currency
+import s from './Currency.module.css';
+import Loader from '../../common/Loader/Loader';
+
+const Currency = () => {
+  const dispatch = useDispatch();
+  const { data: currency, loading, error } = useSelector((state) => state.currency);
+
+  useEffect(() => {
+    dispatch(fetchCurrencyIfNeeded());
+  }, [dispatch]);
+
+  if (loading) return <Loader />;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <section className={s.container}>
+      <table>
+        <thead>
+          <tr>
+            <th>Currency</th>
+            <th>Purchase</th>
+            <th>Sale</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currency.map((item, index) => (
+            <tr key={index}>
+              <td>{item.currencyCodeA}</td>
+              <td>{formatValue(item.rateBuy)}</td>
+              <td>{formatValue(item.rateSell)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <img
+        src={'/img/wallet_hand_up_1x.webp'}
+        srcSet={`/img/wallet_hand_up_2x.webp 2x`}
+        alt={'wallet'}
+        className={s.walletImg}
+      />
+    </section>
+  );
+};
+
+export default Currency;
