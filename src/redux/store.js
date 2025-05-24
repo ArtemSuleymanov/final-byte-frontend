dc2175d783e58bf13384a123e5523a87e666c2d8
Bohdan Potopliak <bohdanpotopliak@gmail.com>
2025-05-24 16:53:29 +0300

    Add Loader

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import loaderReducer from './loader/loaderSlice';
 
 const dummyReducer = (state = {}, action) => state;
 
 export const store = configureStore({
   reducer: {
     dummy: dummyReducer,
+    loader: loaderReducer,
   },
 });
