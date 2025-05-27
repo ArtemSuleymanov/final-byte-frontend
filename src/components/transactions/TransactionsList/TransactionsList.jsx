296f3d249f0c6351e807aeea99c0f0788171192f
Kateryna Kolodynska <kolodinkata@gmail.com>
2025-05-27 21:36:15 +0200

    add scroll for mor 10 transaction

--- src/components/transactions/TransactionsList/TransactionsList.jsx
+++ src/components/transactions/TransactionsList/TransactionsList.jsx
@@ -7,14 +7,15 @@ import {
   selectTransactions,
   selectPagination,
 } from '../../../redux/transactions/transactionsSelectors';
+import Loader from '../../common/Loader/Loader';
 import { useEffect } from 'react';
 
 const TransactionsList = () => {
   const data = useSelector(selectTransactions);
-  // const isLoading = useSelector(selectIsLoading);
+  const isLoading = useSelector(selectIsLoading);
   const paginationInfo = useSelector(selectPagination);
   const dispatch = useDispatch();
-  const isLoading = useSelector((state) => state.loader.isLoading);
+  // const isLoading = useSelector((state) => state.loader.isLoading);
   const { page, hasNextPage } = paginationInfo || {};
 
   useEffect(() => {
@@ -25,32 +26,39 @@ const TransactionsList = () => {
     dispatch(getTransactions(page + 1));
   };
 
-  if (!isLoading && (!Array.isArray(data) || data.length === 0)) {
-    return <p className={s.placeholder}>No transactions yet</p>;
-  }
+  // if (!isLoading && (!Array.isArray(data) || data.length === 0)) {
+  //   return <p className={s.placeholder}>No transactions yet</p>;
+  // }
   return (
     <section className={s.section}>
-      <ul className={s.ul}>
-        <li className={s.header}>
-          <div className={s.wrapper}>
-            <div className={s.date}>Date</div>
-          </div>
-          <div className={s.wrapper}>Type</div>
-          <div className={s.wrapper}>Category</div>
-          <div className={s.wrapper}>Comment</div>
-          <div className={s.wrapper}>Sum</div>
-        </li>
-      </ul>
-      <ul className={s.list}>
-        {(data || []).map((item, index) => (
-          <TransactionsItem key={item._id} {...item} isEven={index % 2 === 1} />
-        ))}
-        {hasNextPage && (
-          <button className={s.moreBtn} onClick={handleLoadMore}>
-            Load more
-          </button>
-        )}
-      </ul>
+      <Loader />
+
+      {Array.isArray(data) && data.length === 0 && !isLoading && <p className={s.placeholder}>No transactions yet</p>}
+      {Array.isArray(data) && data.length > 0 && (
+        <>
+          <ul className={s.ul}>
+            <li className={s.header}>
+              <div className={s.wrapper}>
+                <div className={s.date}>Date</div>
+              </div>
+              <div className={s.wrapper}>Type</div>
+              <div className={s.wrapper}>Category</div>
+              <div className={s.wrapper}>Comment</div>
+              <div className={s.wrapper}>Sum</div>
+            </li>
+          </ul>
+          <ul className={s.list}>
+            {(data || []).map((item, index) => (
+              <TransactionsItem key={item._id} {...item} isEven={index % 2 === 1} />
+            ))}
+            {hasNextPage && (
+              <button className={s.moreBtn} onClick={handleLoadMore}>
+                Load more
+              </button>
+            )}
+          </ul>
+        </>
+      )}
     </section>
   );
 };
